04575f84bc9529d46ffadf76e17145d1f8f09988
RAGHAVENDRA SURYA T.R <88027374+scoder5@users.noreply.github.com>
2022-09-21 15:57:51 +0530

    Time: 33 ms (89.38%), Space: 13.9 MB (6.72%) - LeetHub

--- 319-bulb-switcher/319-bulb-switcher.py
+++ 319-bulb-switcher/319-bulb-switcher.py
@@ -2,9 +2,5 @@ from math import sqrt
 
 class Solution:
     def bulbSwitch(self, n: int) -> int:
-        if not n:
-            return 0
-        if n == 1:
-            return 1
         return int(sqrt(n))
             
