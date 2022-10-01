a34644972a6b4e0109a01c64b368d6056402e12c
RAGHAVENDRA SURYA T.R <88027374+scoder5@users.noreply.github.com>
2022-10-01 08:31:34 +0530

    Time: 790 ms (5.99%), Space: 15 MB (17.74%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
@@ -1,12 +1,9 @@
 class Solution:
     def checkXMatrix(self, grid: List[List[int]]) -> bool:
-        def isDiagonal(i, j, ROWS):
-            return (i == j) or (i + j == ROWS - 1)
-        
         ROWS, COLS = len(grid), len(grid[0])
         for i in range(ROWS):
             for j in range(COLS):
-                if isDiagonal(i, j, ROWS):
+                if j == i or j == ROWS-i-1:
                     if grid[i][j] == 0:
                         return False
                 else:
