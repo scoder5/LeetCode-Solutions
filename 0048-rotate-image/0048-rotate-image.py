c0c19ac0d375c75127003b83ed460263fcd3e74e
RAGHAVENDRA SURYA T.R <88027374+scoder5@users.noreply.github.com>
2022-10-31 16:08:18 +0530

    Time: 57 ms (70.48%), Space: 14 MB (30.06%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,15 +3,15 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        l, r = 0, len(matrix[0]) - 1
+        l, r = 0, len(matrix[0])-1
         while l < r:
             for i in range(r-l):
-                top, bottom = l, r
+                top, bot = l, r
                 topLeft = matrix[top][l+i]
-                matrix[top][l+i] = matrix[bottom-i][l]
-                matrix[bottom-i][l] = matrix[bottom][r-i]
-                matrix[bottom][r-i] = matrix[top+i][r]
+                matrix[top][l+i] = matrix[bot-i][l]
+                matrix[bot-i][l] = matrix[bot][r-i]
+                matrix[bot][r-i] = matrix[top+i][r]
                 matrix[top+i][r] = topLeft
             l += 1
             r -= 1
-            
+        
