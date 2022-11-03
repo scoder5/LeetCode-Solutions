8eb0f1a7f9a6eedb2b73d8cfc41e9b9b97ed3a32
RAGHAVENDRA SURYA T.R <88027374+scoder5@users.noreply.github.com>
2022-11-03 21:56:13 +0530

    Time: 145 ms (41.96%), Space: 15 MB (68.95%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pmap = {}
+        for i, n in enumerate(nums):
+            d = target - n
+            if d in pmap:
+                return [pmap[d], i]
+            pmap[n] = i
