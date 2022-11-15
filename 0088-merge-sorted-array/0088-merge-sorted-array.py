7c53d108362e2652729f7e1ab56fc0670e59b7e3
RAGHAVENDRA SURYA T.R <88027374+scoder5@users.noreply.github.com>
2022-11-15 20:30:31 +0530

    Time: 44 ms (86.44%), Space: 14 MB (38.83%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,7 +4,7 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         last = m + n - 1
-        while m > 0 and  n > 0:
+        while m > 0 and n > 0:
             if nums1[m-1] > nums2[n-1]:
                 nums1[last] = nums1[m-1]
                 m -= 1
